c871e19cf5cc5ebe9af040ad9503511fdb465804
Oli Sturm <oliver@oliversturm.com>
2022-06-06 13:33:40 +0100

    csb hosting env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server:
+    process.env.HOSTING_ENV === 'csb'
+      ? {
+          hmr: {
+            port: 443,
+          },
+        }
+      : {},
+});
